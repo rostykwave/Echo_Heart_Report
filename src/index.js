0b08b3142e4e7d0859ce88bc9032b7a866d84d4c
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-22 10:11:31 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './sass/main.scss';
+
+const refs = {
+    form: document.querySelector('#form'),
+}
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    const formDataRaw = new FormData(e.currentTarget);
+    console.log(formDataRaw);
+
+    const formData = {};
+
+    formDataRaw.forEach((value, key) => {
+        if (value === '') {
+            alert('Всі поля повинні бути заповнені');
+            throw 'Break';
+        }
+
+        formData[key] = value;
+    })
+    console.log(formData);
+
+}
+
