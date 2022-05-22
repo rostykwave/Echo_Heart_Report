35921070fcb1cc490560d981710de84431bf7445
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-22 11:01:21 +0300

    rvResult

--- src/index.js
+++ src/index.js
@@ -4,25 +4,81 @@ const refs = {
     form: document.querySelector('#form'),
 }
 
-refs.form.addEventListener('submit', onSubmit);
+let rvResult;
 
+
+
+refs.form.addEventListener('submit', onSubmit);
+// const formData = {};
+
+
+
+
+
+
+///Main function
 function onSubmit(e) {
     e.preventDefault();
 
     const formDataRaw = new FormData(e.currentTarget);
-    console.log(formDataRaw);
 
     const formData = {};
 
     formDataRaw.forEach((value, key) => {
-        if (value === '') {
-            alert('Всі поля повинні бути заповнені');
-            throw 'Break';
-        }
+        // if (value === '') {
+        //     alert('Всі поля повинні бути заповнені');
+        //     throw 'Break';
+        // }
 
         formData[key] = value;
     })
     console.log(formData);
 
+
+    //Destructuring
+    const { rv, la } = formData;
+
+    // console.log(rv);
+
+    // console.log(typeof(rv));
+    // console.log(typeof(Number(rv)));
+
+    // return formData;
+
+    // let functionResult;
+    // let alertNotification;
+
+
+    rvResult = dimentionCheck(Number(rv), 0.9, 2.6);
+
+    console.log(rvResult);
+  
+
 }
 
+///Additional functions
+
+function dimentionCheck(heartPart, min, max) {
+
+    switch (true) {
+        case isNaN(heartPart):
+            alertNotification = 'Введіть числове значення';
+            break;
+        case heartPart <= 0:
+            // alertNotification = 'Введіть додатнє число';
+            alert('Введіть додатнє число');
+            break;
+        case heartPart > max:
+            return 'D';
+            break;
+        case heartPart < min:
+            return 'S';
+            break;
+    
+        default:
+            return 'N';
+            // functionResult = 'N';
+            alertNotification = '';
+            break;
+    }
+}
