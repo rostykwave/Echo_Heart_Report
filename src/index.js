8e4eb6a5c2b08e7be9c0b825e3050f50aae0f238
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-28 14:19:58 +0300

    commaDotFix pattern

--- src/index.js
+++ src/index.js
@@ -316,7 +316,17 @@ function oneValveCalcification(valveName, valveResult) {
 }
 ///Функція яка бачить рядок ", ." заміняє на ". "
 function comaDotFix(propString) {
-    if (propString.endsWith(', . ')) {
-        return propString.replace(", . ", ". ");
+    // if (propString.endsWith(', . ')) {
+    //     return propString.replace(", . ", ". ");
+    // }
+
+    const pattern = /, . $/ig;
+
+    if (pattern.test(propString)) {
+        return propString.replace(pattern, ". ");
     }
+
+    console.log('Рядок не пройшов перевірку патерном: ', propString.match(pattern));
+    console.log(pattern.test(propString));
+
 }
