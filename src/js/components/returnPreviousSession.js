aa867e9df4c3b6af18f1fae8e7fcdcfd2c83f20c
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-29 13:53:00 +0300

    add styles button

--- src/js/components/returnPreviousSession.js
+++ src/js/components/returnPreviousSession.js
@@ -1,28 +1,26 @@
-import { refs } from "../refs";
+import { refs } from '../refs';
 
-const buttonref = document.querySelector('.return-result');
+const buttonref = document.querySelector('#return-result');
 buttonref.addEventListener('click', returnPreviousSession);
 
 function returnPreviousSession() {
-    const localResult = localStorage.getItem('result');
-    const localEhealthCopyResult = localStorage.getItem('ehealthCopyResult');
-    const localFormData = JSON.parse(localStorage.getItem('formData'));
+  const localResult = localStorage.getItem('result');
+  const localEhealthCopyResult = localStorage.getItem('ehealthCopyResult');
+  const localFormData = JSON.parse(localStorage.getItem('formData'));
 
-    if (localResult && localEhealthCopyResult) {
-         refs.result.textContent = localResult;
-         refs.copyRes.textContent = localEhealthCopyResult;
+  if (localResult && localEhealthCopyResult) {
+    refs.result.textContent = localResult;
+    refs.copyRes.textContent = localEhealthCopyResult;
+  }
+
+  if (localFormData) {
+    console.log(localFormData);
+
+    for (const key in localFormData) {
+      if (Object.hasOwnProperty.call(localFormData, key)) {
+        const element = localFormData[key];
+        console.log(key + ': ' + element);
+      }
     }
-
-    if (localFormData) {
-        console.log(localFormData);
-
-
-        for (const key in localFormData) {
-         
-            if (Object.hasOwnProperty.call(localFormData, key)) {
-                const element = localFormData[key];
-                console.log(key +": "+ element);
-            }
-        }
-    }
+  }
 }
