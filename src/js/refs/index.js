009ef77b2405cf52d95c1f515a1cc2c2c15f5607
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-30 14:17:25 +0300

    Update index.js

--- src/js/refs/index.js
+++ src/js/refs/index.js
@@ -1,5 +1,10 @@
 export const refs = {
-    form: document.querySelector('#form'),
-    result: document.querySelector('#result'),
-    copyRes: document.querySelector('#copy-result'),
-}
+  form: document.querySelector('#form'),
+  result: document.querySelector('#result'),
+  copyRes: document.querySelector('#copy-result'),
+  saveBtn: document.querySelector('#save-form-btn'),
+  clearBtn: document.querySelector('#clear-form-btn'),
+  inputSurname: document.querySelector('#surname'),
+  inputName: document.querySelector('#name'),
+  inputSecondName: document.querySelector('#secondname'),
+};
