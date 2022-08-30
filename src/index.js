dfd457914b0b95b3e22e8cba9a42d0deffcbbfa1
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-30 14:17:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { refs } from './js/refs';
 
 ///Helpers
 import { comaDotFix } from './js/helpers/comaDotFix';
-import {onRightAndLeftArrows} from './js/helpers/onRightAndLeftArrows';
+import { onRightAndLeftArrows } from './js/helpers/onRightAndLeftArrows';
 ///////\\Helpers
 
 ////Valves
@@ -21,14 +21,11 @@ import { diastolicRes } from './js/components/diastolic';
 import { segmentsRes } from './js/components/segments';
 import { segmentsRender } from './js/components/segments';
 
-
-
 import { efREs } from './js/components/ef';
 
-import { copyResult } from './js/result/copyResult'; 
+import { copyResult } from './js/result/copyResult';
 import { formDataStore, resultStore } from './js/helpers/loacalStorage';
 import returnPreviousSession from './js/components/returnPreviousSession';
-
 
 let rvResult = 'Spare';
 let laResult = 'Spare';
@@ -44,229 +41,239 @@ let val = 'Spare';
 // let segmDisplay = 'Spare';
 let result = 'Spare';
 
-
 refs.form.addEventListener('submit', onSubmit);
+refs.saveBtn.addEventListener('click', onSaveClick);
+refs.clearBtn.addEventListener('click', onClearClick);
 ///Переміщення між інпутами
 document.addEventListener('keydown', onRightAndLeftArrows);
 
-
 ///Main function
+
 function onSubmit(e) {
-    e.preventDefault();
-    const formDataRaw = new FormData(e.currentTarget);
-    const formData = {};
-    formDataRaw.forEach((value, key) => {
-        formData[key] = value;
-    })
-    console.log(formData);
+  e.preventDefault();
+  const formDataRaw = new FormData(e.currentTarget);
+  const formData = {};
+  formDataRaw.forEach((value, key) => {
+    formData[key] = value;
+  });
+  // console.log(formData);
 
-    formDataStore(formData);
+  formDataStore(formData);
 
-    //Destructuring
-    const { rv, la, aorta, ivs, lvWall, lv, ef } = formData;
+  //Destructuring
+  const { rv, la, aorta, ivs, lvWall, lv, ef } = formData;
 
-    rvResult = dimentionCheck(Number(rv), 0.9, 2.6);
-    laResult = dimentionCheck(Number(la), 1.9, 4);
-    aortaResult = dimentionCheck(Number(aorta), 2, 3.7);
-    ivsResult = dimentionCheck(Number(ivs), 0.6, 1.1);
-    lvWallResult = dimentionCheck(Number(lvWall), 0.6, 1.1);
-    lvResult = dimentionCheck(Number(lv), 3.5, 5.7);
+  rvResult = dimentionCheck(Number(rv), 0.9, 2.6);
+  laResult = dimentionCheck(Number(la), 1.9, 4);
+  aortaResult = dimentionCheck(Number(aorta), 2, 3.7);
+  ivsResult = dimentionCheck(Number(ivs), 0.6, 1.1);
+  lvWallResult = dimentionCheck(Number(lvWall), 0.6, 1.1);
+  lvResult = dimentionCheck(Number(lv), 3.5, 5.7);
 
+  /////Стінки
 
-    /////Стінки
+  wallResult = evaluateHeartWall(ivsResult, lvWallResult);
+  // console.log(wallResult);
 
-    wallResult = evaluateHeartWall(ivsResult, lvWallResult);
-    // console.log(wallResult);
+  ///Камери
 
-    ///Камери
+  chamberResult = evaluateHeartChamber(rvResult, laResult, aortaResult, lvResult);
+  // console.log(chamberResult);
 
-    chamberResult = evaluateHeartChamber(rvResult, laResult, aortaResult, lvResult);
-    // console.log(chamberResult);
+  const ch = evaluateMainResult(wallResult, chamberResult);
 
-    const ch = evaluateMainResult(wallResult, chamberResult);
+  ////Клапани
+  ///Недостатність клапанів///////
+  const { mInsuf, aInsuf, tInsuf, laInsuf } = formData;
+  const valInsuf = evaluateValvesInsufficiency(mInsuf, aInsuf, tInsuf, laInsuf);
 
+  ///Кальциноз клапанів///////
+  const { mCalc, aCalc, tCalc, laCalc } = formData;
+  const valCalc = evaluateValvesCalcification(mCalc, aCalc, tCalc, laCalc);
 
-    ////Клапани
-    ///Недостатність клапанів///////
-    const { mInsuf, aInsuf, tInsuf, laInsuf } = formData;
-    const valInsuf = evaluateValvesInsufficiency(mInsuf, aInsuf, tInsuf, laInsuf);
+  ////ADD клапанів///////
+  const { mAdd, aAdd, tAdd, laAdd } = formData;
+  const valvAdd = valvesAddInfo(mAdd, aAdd, tAdd, laAdd);
 
-     ///Кальциноз клапанів///////
-    const { mCalc, aCalc, tCalc, laCalc } = formData;
-    const valCalc = evaluateValvesCalcification(mCalc, aCalc, tCalc, laCalc);
+  ///Загальний звіт по клапанах\
+  const val = valInsuf + valCalc + valvAdd;
 
-    ////ADD клапанів///////
-    const { mAdd, aAdd, tAdd, laAdd } = formData;
-    const valvAdd = valvesAddInfo(mAdd, aAdd, tAdd, laAdd);
+  const { pulmonaryHypertension, perFluid, diastolDysf, diastolDysfPlus } = formData;
 
-   
+  // new consts
 
-    ///Загальний звіт по клапанах\
-    const val = valInsuf + valCalc + valvAdd;
+  const pulmoHypertResult = pulmoHypert(pulmonaryHypertension);
 
+  const pericardialFluidResult = pericardialFluidRes(perFluid);
+  const diastolicResult = diastolicRes(diastolDysf, diastolDysfPlus);
 
+  const segmentResult = segmentsRes(formData, Number(ef));
+  const efResult = efREs(ef);
+  const segmTextCopy = segmentsRender(formData);
 
-    const { pulmonaryHypertension, perFluid, diastolDysf, diastolDysfPlus } = formData;
+  ///загальний висновок
+  result = resultOutput(
+    ch,
+    val,
+    pulmoHypertResult,
+    pericardialFluidResult,
+    diastolicResult,
+    segmentResult,
+    efResult,
+  );
+  const ehealthCopyResult = copyResult(formData, result, segmTextCopy);
+  // console.log(result);
 
-    // new consts
-    
-    const pulmoHypertResult = pulmoHypert(pulmonaryHypertension);
+  resultStore({ result, ehealthCopyResult });
 
-    const pericardialFluidResult = pericardialFluidRes(perFluid);
-    const diastolicResult = diastolicRes(diastolDysf, diastolDysfPlus);
+  refs.result.textContent = result;
+  ///Висновок для копіювання в медичний звіт
+  // const copyResult = 'ggdg';
+  refs.copyRes.textContent = ehealthCopyResult;
+}
 
-    const segmentResult = segmentsRes(formData, Number(ef));
-    const efResult = efREs(ef);
-    const segmTextCopy = segmentsRender(formData);
-    
+///Function to save patient input fields
 
+function onSaveClick() {
+  //TODO
+}
 
-    ///загальний висновок
-    result = resultOutput(ch, val, pulmoHypertResult, pericardialFluidResult, diastolicResult, segmentResult, efResult);
-    const ehealthCopyResult = copyResult(formData, result, segmTextCopy);
-    // console.log(result);
+///Function to clear patient input fields
 
-    resultStore({result, ehealthCopyResult});
-
-
-    refs.result.textContent = result;
-    ///Висновок для копіювання в медичний звіт
-    // const copyResult = 'ggdg';
-    refs.copyRes.textContent = ehealthCopyResult;
+function onClearClick() {
+  refs.inputSurname.value = '';
+  refs.inputName.value = '';
+  refs.inputSecondName.value = '';
 }
 
 ///Additional functions
 
 function dimentionCheck(heartPart, min, max) {
+  switch (true) {
+    case isNaN(heartPart):
+      Report.info('Увага', `Введіть числове значення у поле з вмістом "${heartPart}"`);
+      break;
+    case heartPart < 0:
+      Report.info('Увага', `Введіть додатнє число у поле з вмістом "${heartPart}"`);
+      break;
+    case heartPart === 0:
+      Report.info('Увага', `Заповніть поле з пустим вмістом`);
+      return '';
+      break;
+    case heartPart > max:
+      return 'D';
+      break;
+    case heartPart < min:
+      return 'S';
+      break;
 
-    switch (true) {
-        case isNaN(heartPart):
-              Report.info('Увага', `Введіть числове значення у поле з вмістом "${heartPart}"`);
-            break;
-        case heartPart < 0:
-            Report.info('Увага', `Введіть додатнє число у поле з вмістом "${heartPart}"`);
-            break;
-        case heartPart === 0:
-            Report.info('Увага', `Заповніть поле з пустим вмістом`);
-            return '';
-            break;
-        case heartPart > max:
-            return 'D';
-            break;
-        case heartPart < min:
-            return 'S';
-            break;
-    
-        default:
-            return 'N';
-            break;
-    }
+    default:
+      return 'N';
+      break;
+  }
 }
 
-
 function evaluateHeartWall(ivs, lvWall) {
-    switch (true) {
-        case ivs === '' || lvWall === '':
-            return "";
-            break;
-        
-        case ivs === 'D' && lvWall === 'D':
-            return "Гіпертрофія стінок лівого шлуночка. ";
-            break;
-        
-        case ivs === 'D' && lvWall !== 'D':
-            return "Гіпертрофія міжшлуночкової перегородки. ";
-            break;
-        
-        case ivs !== 'D' && lvWall === 'D':
-            return "Гіпертрофія задньої стінки лівого шлуночка. ";
-            break;
-    
-        default:
-            return 'Normal';
-            break;
-    }
+  switch (true) {
+    case ivs === '' || lvWall === '':
+      return '';
+      break;
+
+    case ivs === 'D' && lvWall === 'D':
+      return 'Гіпертрофія стінок лівого шлуночка. ';
+      break;
+
+    case ivs === 'D' && lvWall !== 'D':
+      return 'Гіпертрофія міжшлуночкової перегородки. ';
+      break;
+
+    case ivs !== 'D' && lvWall === 'D':
+      return 'Гіпертрофія задньої стінки лівого шлуночка. ';
+      break;
+
+    default:
+      return 'Normal';
+      break;
+  }
 }
 
 function evaluateHeartChamber(rv, la, aorta, lv) {
-    let rvText = '';
-    let laText = '';
-    let aortaText = '';
-    let lvText = '';
+  let rvText = '';
+  let laText = '';
+  let aortaText = '';
+  let lvText = '';
 
+  switch (true) {
+    case rv === '' || la === '' || aorta === '' || lv === '':
+      return '';
+      break;
 
-    switch (true) {
-        case rv === '' || la === '' || aorta === '' || lv === '':
-            return ""
-            break;
-        
-        case rv === 'D' && la === 'D' && aorta === 'D' && lv === 'D':
-            return "Дилятація всіх камер серця. "
-            break;
-        
-        case rv === 'D' && la === 'D' && aorta !== 'D' && lv === 'D':
-            return "Дилятація лівого передсердя та шлуночків серця. "
-            break;
-        
-        case rv === 'D' && la !== 'D' && aorta === 'D' && lv === 'D':
-            return "Дилятація аорти та шлуночків серця. "
-            break;
-        
-        case rv === 'D' && la !== 'D' && aorta !== 'D' && lv === 'D':
-            return "Дилятація шлуночків серця. "
-            break;
-        
-        case rv !== 'D' && la === 'D' && aorta !== 'D' && lv === 'D':
-            return "Дилятація лівих камер серця. "
-            break;
-        
-        ////коли хоч якась камера диляьована, і коли жодна з попередніх умов не задовольнила
-        case rv === 'D' || la === 'D' || aorta === 'D' || lv === 'D':
+    case rv === 'D' && la === 'D' && aorta === 'D' && lv === 'D':
+      return 'Дилятація всіх камер серця. ';
+      break;
 
-            if (rv === 'D') {
-                rvText = "правого  шлуночка, "; 
-            }
-            if (la === 'D') {
-                laText = "лівого передсердя, "; 
-            }
-            if (aorta === 'D') {
-                aortaText = "висхідного відділу аорти, "; 
-            }
-            if (lv === 'D') {
-                lvText = "лівого  шлуночка, "; 
-            }
+    case rv === 'D' && la === 'D' && aorta !== 'D' && lv === 'D':
+      return 'Дилятація лівого передсердя та шлуночків серця. ';
+      break;
 
-            return comaDotFix(`Дилятація ${rvText}${laText}${aortaText}${lvText}. `);
-            break;
-    
-        default:
-            return 'Normal';
-            break;
-    }
+    case rv === 'D' && la !== 'D' && aorta === 'D' && lv === 'D':
+      return 'Дилятація аорти та шлуночків серця. ';
+      break;
+
+    case rv === 'D' && la !== 'D' && aorta !== 'D' && lv === 'D':
+      return 'Дилятація шлуночків серця. ';
+      break;
+
+    case rv !== 'D' && la === 'D' && aorta !== 'D' && lv === 'D':
+      return 'Дилятація лівих камер серця. ';
+      break;
+
+    ////коли хоч якась камера диляьована, і коли жодна з попередніх умов не задовольнила
+    case rv === 'D' || la === 'D' || aorta === 'D' || lv === 'D':
+      if (rv === 'D') {
+        rvText = 'правого  шлуночка, ';
+      }
+      if (la === 'D') {
+        laText = 'лівого передсердя, ';
+      }
+      if (aorta === 'D') {
+        aortaText = 'висхідного відділу аорти, ';
+      }
+      if (lv === 'D') {
+        lvText = 'лівого  шлуночка, ';
+      }
+
+      return comaDotFix(`Дилятація ${rvText}${laText}${aortaText}${lvText}. `);
+      break;
+
+    default:
+      return 'Normal';
+      break;
+  }
 }
 function evaluateMainResult(wall, chamber) {
-    switch (true) {
-        case wall === 'Normal' && chamber === 'Normal':
-            return "Розміри камер серця в межах норми. "
-            break;
-        
-        case wall !== 'Normal' && chamber === 'Normal':
-            return wall;
-            break;
-        
-        case wall === 'Normal' && chamber !== 'Normal':
-            return chamber;
-            break;
-    
-        default:
-            return chamber + wall;
-            break;
-    }
+  switch (true) {
+    case wall === 'Normal' && chamber === 'Normal':
+      return 'Розміри камер серця в межах норми. ';
+      break;
+
+    case wall !== 'Normal' && chamber === 'Normal':
+      return wall;
+      break;
+
+    case wall === 'Normal' && chamber !== 'Normal':
+      return chamber;
+      break;
+
+    default:
+      return chamber + wall;
+      break;
+  }
 }
 
 function resultOutput(ch, val, pulmo, pericardFluid, diastolic, segm, ef) {
-    if (segm.startsWith('Порушення')) {
-       return segm + ef + ch + val + pulmo + pericardFluid + diastolic; 
-    }
-    return ch + val + pulmo + pericardFluid+ diastolic + segm + ef;
+  if (segm.startsWith('Порушення')) {
+    return segm + ef + ch + val + pulmo + pericardFluid + diastolic;
+  }
+  return ch + val + pulmo + pericardFluid + diastolic + segm + ef;
 }
